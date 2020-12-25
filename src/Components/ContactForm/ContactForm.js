b89d9a74c5c2240eabe12a6ec02f21284d51d9d8
Andrii Stanislav <stanislav.andrii@gmail.com>
2020-12-25 10:43:33 +0200

    Fix bugs

--- src/Components/ContactForm/ContactForm.js
+++ src/Components/ContactForm/ContactForm.js
@@ -5,47 +5,57 @@ import { v4 as uuidv4 } from 'uuid';
 import styles from './ContactForm.module.css';
 
 class ContactForm extends Component {
-    state = {
-        name: '',
-        number: '',
-    }
-    
-    heandleInput = (event) => {
-        this.setState({ [event.currentTarget.name]: event.currentTarget.value, });   
-    }
+  state = {
+    name: '',
+    number: '',
+  };
 
-    createNewContact = (event) => {
-        event.preventDefault();
+  heandleInput = event => {
+    this.setState({ [event.currentTarget.name]: event.currentTarget.value });
+  };
 
-        this.props.onSubmit({ id: uuidv4() , ...this.state, })
-    
-        this.setState({
-            name: '',
-            number: '',
-        })      
-    }
+  createNewContact = event => {
+    event.preventDefault();
 
-    render() {
-        return <form className={styles.form} onSubmit={ this.createNewContact }>
-            <label className={styles.label}>
-                Name:
-                <input className={styles.input} name="name" type="text" value={this.state.name} onChange={this.heandleInput} />
-            </label>
-            <label className={styles.label}> 
-                Phone <span className={styles.example}>(000-00-00)</span>:
-                <input className={styles.input}
-                    name="number"
-                    type="tel"
-                    pattern="[0-9]{3}-[0-9]{2}-[0-9]{2}"
-                    maxLength="9"
-                    value={this.state.number}
-                    onChange={this.heandleInput} />
-            </label>
-            <button className={styles.button} type="submit">Create contact</button>
-        </form>
-    }
+    this.props.onSubmit({ id: uuidv4(), ...this.state });
+
+    this.setState({
+      name: '',
+      number: '',
+    });
+  };
+
+  render() {
+    return (
+      <form className={styles.form} onSubmit={this.createNewContact}>
+        <label className={styles.label}>
+          Name:
+          <input
+            className={styles.input}
+            name="name"
+            type="text"
+            value={this.state.name}
+            onChange={this.heandleInput}
+          />
+        </label>
+        <label className={styles.label}>
+          Phone <span className={styles.example}>(000-00-00)</span>:
+          <input
+            className={styles.input}
+            name="number"
+            type="tel"
+            pattern="[0-9]{3}-[0-9]{2}-[0-9]{2}"
+            maxLength="9"
+            value={this.state.number}
+            onChange={this.heandleInput}
+          />
+        </label>
+        <button className={styles.button} type="submit">
+          Create contact
+        </button>
+      </form>
+    );
+  }
 }
 
-
 export default ContactForm;
-
--- src/Components/ContactList/ContactList.js
+++ src/Components/ContactList/ContactList.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Contact from './Contact'
+import Contact from './Contact';
 
 import styles from './ContactList.module.css';
 
-const ContactList = ({contacts, deleteOnClick}) => {
-    return <ul className={styles.list}>
-        {contacts.map(({ id, name, number }) => (
-            <li className={styles.item} key={id}>
-                <Contact name={name} number={number} />
-                <button className={styles.button} id={id} onClick={ deleteOnClick }>Delete</button>
-            </li>))}
-   </ul>
-}
+const ContactList = ({ contacts, deleteOnClick }) => {
+  return (
+    <ul className={styles.list}>
+      {contacts.map(({ id, name, number }) => (
+        <li className={styles.item} key={id}>
+          <Contact name={name} number={number} />
+          <button className={styles.button} id={id} onClick={deleteOnClick}>
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 ContactList.propTypes = {
-    contacts: PropTypes.array.isRequired,
-    deleteOnClick: PropTypes.func.isRequired,
-}
+  contacts: PropTypes.array.isRequired,
+  deleteOnClick: PropTypes.func.isRequired,
+};
 
 export default ContactList;
